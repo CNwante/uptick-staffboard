b043d7a5c7c183185c369425254196ce784c2707
Solomon Nwante <solomonchuks202@gmail.com>
2025-08-05 17:02:17 +0100

    Add clsx for dynamic styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import type { ButtonProps } from "./types";
 
 export const Button: React.FC<ButtonProps> = ({
@@ -10,7 +11,7 @@ export const Button: React.FC<ButtonProps> = ({
     <button
       type={type}
       onClick={onClick}
-      className={`bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 ${className}`}
+      className={clsx("bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700", className)}
     >
       {children}
     </button>
